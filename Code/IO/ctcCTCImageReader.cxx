a3d658b199b71f69fd6f78f7e1302572aeede18a
Chris Wyatt <clwyatt@vt.edu>
2007-02-28 09:28:14 +0000

    CLW: added check for no DICOM series found, plus some code cleanup

--- Code/IO/ctcCTCImageReader.cxx
+++ Code/IO/ctcCTCImageReader.cxx
@@ -48,13 +48,18 @@ namespace ctc
     typedef std::vector<std::string> seriesIdContainer;
     const seriesIdContainer & seriesUID = nameGenerator->GetSeriesUIDs();
 
+    if(seriesUID.empty())
+      {
+	throw itk::ImageFileReaderException(__FILE__, 
+					    __LINE__, 
+					    "No DICOM Series Found");
+      }
+
     seriesIdContainer::const_iterator seriesItr = seriesUID.begin();
     seriesIdContainer::const_iterator seriesEnd = seriesUID.end();
 
     typedef std::vector<std::string> fileNamesContainer;
     fileNamesContainer fileNames;
-
-
     fileNames = nameGenerator->GetFileNames(seriesUID.begin()->c_str());
 
     ReaderType::Pointer reader = ReaderType::New();
@@ -84,62 +89,4 @@ namespace ctc
   }
   
 }
-
-
-
-
-//   void CTCImageReader::DeterminePixelType()
-//   {
-
-//     assert(!m_Directory.empty());
-
-//     itk::GDCMImageIO::Pointer dicomIO = itk::GDCMImageIO::New();
-
-//     // Get the DICOM filenames from the directory
-//     itk::GDCMSeriesFileNames::Pointer nameGenerator = 
-//       itk::GDCMSeriesFileNames::New();
-    
-//     nameGenerator->SetDirectory( m_Directory.c_str() );
-
-//     typedef std::vector<std::string> seriesIdContainer;
-//     const seriesIdContainer & seriesUID = nameGenerator->GetSeriesUIDs();
-
-//     typedef std::vector<std::string> fileNamesContainer;
-//     fileNamesContainer fileNames;
-
-//     fileNames = nameGenerator->GetFileNames(seriesUID.begin()->c_str());
-
-//     fileNames.resize(2);
-
-//     ReaderType::Pointer reader = ReaderType::New();
-//     reader->SetFileNames( fileNames );
-//     reader->SetImageIO( dicomIO );
-
-//     reader->Update();
-
-//     typedef itk::MetaDataDictionary   DictionaryType;
-    
-//     const  DictionaryType & dictionary = dicomIO->GetMetaDataDictionary();
-    
-//     std::string entryId = "0028|0103";
-//     DictionaryType::ConstIterator tagItr = dictionary.Find( entryId );
-    
-//     if( tagItr != dictionary.End() )
-//       {
-//  	typedef itk::MetaDataObject< std::string > MetaDataStringType;
-//  	MetaDataStringType::ConstPointer entryvalue = 
-//  	  dynamic_cast<const MetaDataStringType *>( 
-//  						   tagItr->second.GetPointer() 
-//  						    );
-//  	if( entryvalue )
-//  	  {
-//  	    std::string tagvalue = entryvalue->GetMetaDataObjectValue();
-//  	    std::cout << "Pixel Representation (" << entryId <<  ") ";
-//  	    std::cout << " is: " << tagvalue.c_str() << std::endl;
-//  	  }
-	
-//       }
-//   }
-
-
 
