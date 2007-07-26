236737991503b90d8d1dfbe2dd3743d28fee0806
Chris Wyatt <clwyatt@vt.edu>
2007-07-26 10:56:20 +0000

    CLW: added standalone frontend to colon segmentation code, will eventually allow switching between algorithms

---
+++ Applications/SegmentColon/SegmentColon.cxx
@@ -0,0 +1,101 @@
+/*******************************************************************************
+Program:   Wake Forest University - Virginia Tech CTC Software
+Id:        $Id$
+Language:  C++
+*******************************************************************************/
+
+// command line app to segment colon
+ 
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+// ITK IO includes
+#include "itkImageFileWriter.h"
+
+// CTC includes
+#include "ctcConfigure.h"
+#include "ctcCTCImage.h"
+#include "ctcCTCImageReader.h"
+#include "ctcSegmentColonFilter.h"
+#include "ctcSegmentColonWithContrastFilter.h"
+#include "vul_arg.h"
+
+using namespace std;
+
+// global typedefs
+typedef itk::ImageFileWriter< ctc::BinaryImageType >  WriterType; 
+
+int main(int argc, char ** argv)
+{
+  vul_arg<char const*> infile(0, "Dicom directory");
+  vul_arg<char const*> outfilebase(0, "Output base for filenames (no extension)");
+  vul_arg<bool> segcontrast("-s", "Segment Contrast", false);
+  vul_arg<int> maxiter("-i", "Maximum number of iterations (ignored if -s)", 5);
+  vul_arg<int> mindist("-d", "Minimum distance considered (ignored if -s)", 10);
+  vul_arg<int> thresh("-t", "Air/Tissue threshold (ignored if -s)", -800);
+  vul_arg_parse(argc, argv);
+
+  // read in the DICOM series
+  ctc::CTCImageReader::Pointer reader = ctc::CTCImageReader::New();
+  reader->SetDirectory(string(infile()));
+  try
+    {
+      reader->Update();
+    }
+  catch (itk::ExceptionObject &ex)
+    {
+      std::cout << ex << std::endl;
+      return EXIT_FAILURE;
+    }
+
+  if( !segcontrast() )
+    {
+      // segment air only
+      ctc::SegmentColonFilter::Pointer filter = ctc::SegmentColonFilter::New();
+      filter->SetInput( reader->GetOutput() );
+      filter->SetThreshold(thresh());
+      filter->SetMaxIterations(maxiter());
+      filter->SetMinDistanceThreshold(mindist());
+  
+      // write out binary image
+      WriterType::Pointer writer = WriterType::New();
+      string outfilename1 = string(outfilebase()) + ".vtk";
+      writer->SetFileName(outfilename1.c_str()); 
+      writer->SetInput( filter->GetOutput() );
+      try 
+	{ 
+	  writer->Update(); 
+	} 
+      catch( itk::ExceptionObject & err ) 
+	{ 
+	  cerr << "ExceptionObject caught !" << endl; 
+	  cerr << err << endl; 
+	  return EXIT_FAILURE; 
+	} 
+    }
+  else
+    {
+      // segment air + constrast
+      ctc::SegmentColonWithContrastFilter::Pointer filter = ctc::SegmentColonWithContrastFilter::New();
+      filter->SetInput( reader->GetOutput() );
+  
+      // write out binary image
+      WriterType::Pointer writer = WriterType::New();
+      string outfilename1 = string(outfilebase()) + ".vtk";
+      writer->SetFileName(outfilename1.c_str()); 
+      writer->SetInput( filter->GetOutput() );
+      try 
+	{ 
+	  writer->Update(); 
+	} 
+      catch( itk::ExceptionObject & err ) 
+	{ 
+	  cerr << "ExceptionObject caught !" << endl; 
+	  cerr << err << endl; 
+	  return EXIT_FAILURE; 
+	} 
+    }
+  
+  return(EXIT_SUCCESS);
+}
