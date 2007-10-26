cca65aac6deb19bee533e387a8b00bbcad4c97a6
Yuan Shen <yuan.sh.yuan@gmail.com>
2007-10-26 14:53:38 +0000

    Feature Extraction Program version --- 1.0.0

--- Applications/FeatureExtraction/FeatureExtractionTool.cxx
+++ Applications/FeatureExtraction/FeatureExtractionTool.cxx
@@ -98,46 +98,32 @@ int main( int argc, char* argv[] )
 {
   // parse args
   vul_arg<char const*> imgdir(0, "DICOM image dir");
-  vul_arg<char const*> datainfile(0, "input volume data file", "SupineData.vtk");
-  vul_arg<char const*> seginfile(0, "segmented image", "SupineSegmented.vtk");
-  vul_arg<char const*> outfilename(0, "Output feature filename", "PolypCandidatesExtractedInSegfile.vtk");
-  //vul_arg<char const*> pfile("-p", "polyp coordinate file", "polyps");
-  //vul_arg<char const*> outfilename("-o", 
-		//		   "output csv file name", 
-		//		   "features.csv");
+  vul_arg<char const*> seginfile(0, "Segmented image", "SupineSegmented.vtk/SupineSegmented.img");
+  vul_arg<char const*> outfilename(0, "Output feature filename(no extension)", "PolypCandidatesExtracted");
   vul_arg_parse(argc, argv);
 
 
   /* get paths to input/output files */
   string rawinfilename = "";
   rawinfilename.append(imgdir());
-  rawinfilename.append("/");
-  rawinfilename.append(datainfile());
   string seginfilename = "";
   seginfilename.append(imgdir());
-  seginfilename.append("/");
   seginfilename.append(seginfile());
   string outputVTKname = "";
   outputVTKname.append(outfilename());
 
-  // read data
-  //ctc::CTCImageReader::Pointer rawreader = ctc::CTCImageReader::New();
-  //rawreader->SetDirectory(string(imgdir()));
-  //rawreader->SetFileName(rawinfilename);
-  typedef itk::ImageFileReader< ctc::CTCImageType > RawDataReaderType;
-  RawDataReaderType::Pointer rawreader = RawDataReaderType::New();
-  rawreader->SetFileName(rawinfilename);
-  
+  // read in the DICOM series
+  ctc::CTCImageReader::Pointer reader = ctc::CTCImageReader::New();
+  reader->SetDirectory(rawinfilename);
 
   // read segmented data
   typedef itk::ImageFileReader< ctc::BinaryImageType > SegDataReaderType;
   SegDataReaderType::Pointer segreader = SegDataReaderType::New();
   segreader->SetFileName(seginfilename);
 
-  // invoke readers
   try
     {
-      rawreader->Update();
+      reader->Update();
       segreader->Update();
     }
   catch (itk::ExceptionObject &ex)
@@ -146,11 +132,12 @@ int main( int argc, char* argv[] )
       return EXIT_FAILURE;
     }
 
+
   // extract the features
   typedef ctc::PrincipleCurvatureExtraction FilterType;
   FilterType::Pointer filter = FilterType::New();
   filter->SetBinaryInput(segreader->GetOutput());
-  filter->SetImageInput(rawreader->GetOutput());
+  filter->SetImageInput(reader->GetOutput());
   std::clog << "Computing Features ...";
   try
     {
@@ -163,98 +150,19 @@ int main( int argc, char* argv[] )
     }
   std::clog << " Done." << std::endl;
 
-  // read the polyps file
- 
-  
-  /* Will uncomment soon */
-  /*
-  DBType * db;
-  try
-    {
-      db = parsedb(pfile());
-    }
-  catch(bool stat)
-    {
-      cerr << "Error reading file " << pfile() << endl;
-      return EXIT_FAILURE;
-    }
-  */
-
-  /* Previous Work */
-  /*
-  // output
-  ofstream out(outfilename());
-
-  out << "voxelx" << "," 
-      << "voxely" << ","
-      << "voxelz" << ","
-      << "kappa1" << ","
-      << "kappa2" << ","
-      << "shape_index" << ","
-      << "curvedness" << ","
-      << "gauusian_curv" << ","
-      << "mean_curv" << ","
-      << "dist_to_closest" << ","
-      << "id_of_closest"
-      << endl;
-
-  FilterType::FeatureSampleType::Pointer features = filter->GetOutput();
-  FilterType::FeatureType f;
-  DBType::iterator it;
-  for(int i = 0; i < features->Size(); i++)
-    {
-       f = features->GetMeasurementVector(i);
-       int id = 0;
-       float mindist = pow(512,6);
-       for(it = db->begin(); it != db->end(); ++it)
-	 {
-	   float dx = f[0] - it->x;
-	   float dy = f[1] - it->y;
-	   float dz = f[2] - it->z;
-	   float dist = dx*dx + dy*dy + dz*dz;
-	   if( dist < mindist)
-	     {
-	       mindist = dist;
-	       id = it->id;
-	     }
-	 }
-       mindist = sqrt(mindist);
-
-       //note the si is slightly different, since I use atan2 in place of atan
-       float si = 0.5 + 0.1592*atan2(f[3] + f[4], f[3] - f[4]);
-       float cv = 0.6366*log(sqrt((f[3]*f[3] + f[4]*f[4])/2));
-       float K = f[3]*f[4];
-       float H = 0.5*(f[3] + f[4]);
-
-       out << f[0] << "," 
-	   << f[1] << ","
-	   << f[2] << ","
-	   << f[3] << ","
-	   << f[4] << ","
-	   << si << ","
-	   << cv << ","
-	   << K << ","
-	   << H << ","
-	   << mindist << ","
-	   << id
-	   << endl;
-    }  
-  out.close();*/
  
     typedef ctc::FeatureExtraction FeatureExtractionFilterType;  
     FeatureExtractionFilterType::Pointer FEfilter = FeatureExtractionFilterType::New();
-//  FEfilter->SetInitialFeatureVector(filter->GetOutput());
+    //FEfilter->SetSeedRegion(filter->GetSeedRegion());
+    //FEfilter->SetInitialFeatureVector(filter->GetOutput());
 
     FEfilter->SetSegmentedImageInput(segreader->GetOutput());
-    FEfilter->SetRawImageInput(rawreader->GetOutput());
+    FEfilter->SetRawImageInput(reader->GetOutput());
     FEfilter->SetOutputVTK(outputVTKname);
 
     /* Core of feature extraction */
     FEfilter->Analyze();
-
-
-//  delete db;
-
-  return EXIT_SUCCESS;
+  
+    return EXIT_SUCCESS;
 
 }
